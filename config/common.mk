dabc26287707bcb44bf92b15a912469f3729dc97
AnierinB <anierin@evolution-x.org>
2019-09-15 23:52:51 +0530

    lineage: Initial RyzOS rebrand
    Revert "fixup! fixup! roomservice: Migrate from GitHub API to git ls-remote"
    
    This reverts commit 991fd49.
    
    Revert "fixup! roomservice: Migrate from GitHub API to git ls-remote"
    
    This reverts commit d5aa270.
    
    Revert "roomservice: Migrate from GitHub API to git ls-remote"
    
    This reverts commit 4de0265.
    
    Revert "roomservice: Skip setting revision if it's the same as default"
    
    This reverts commit caae070.
    
    Revert "fixup! roomservice: Skip setting revision if it's the same as default"
    
    This reverts commit 1c00116.
    
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Lup Gabriel <gwolf2u.website@gmail.com>
    Co-authored-by: Hemant Sachdeva <hemant.evolver@gmail.com>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Change-Id: I35e1b706cf3319212d4f723dd95fcb3126dd85fb
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -1,7 +1,7 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 
-PRODUCT_BRAND ?= LineageOS
+PRODUCT_BRAND ?= RyzOS
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -87,9 +87,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.android.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.android.xml
 
-# Enforce privapp-permissions whitelist
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.control_privapp_permissions=enforce
+# Enable transitional log for Privileged permissions
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.control_privapp_permissions=log
 
 ifneq ($(TARGET_DISABLE_LINEAGE_SDK), true)
 # Lineage SDK
@@ -115,8 +115,6 @@ endif
 # Bootanimation
 TARGET_SCREEN_WIDTH ?= 1080
 TARGET_SCREEN_HEIGHT ?= 1920
-PRODUCT_PACKAGES += \
-    bootanimation.zip
 
 # Build Manifest
 PRODUCT_PACKAGES += \
@@ -136,8 +134,7 @@ PRODUCT_PACKAGES += \
 endif
 
 PRODUCT_PACKAGES += \
-    LineageSettingsProvider \
-    Updater
+    LineageSettingsProvider
 
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
@@ -268,7 +265,7 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 include vendor/lineage/config/version.mk
 
--include vendor/lineage-priv/keys/keys.mk
+# Private keys
+-include vendor/ryz-priv/keys/keys.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
--include vendor/lineage/config/partner_gms.mk
