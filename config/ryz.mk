68175c90c5ceb6b9b4a1ea8df9482c86912ca790
ponces <ponces26@gmail.com>
2023-11-15 16:02:38 +0000

    vendor: Switch to AOSPA's face unlock implementation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/ryz.mk
+++ config/ryz.mk
@@ -1,3 +1,12 @@
 # Cloned app exemption
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-ryz-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-ryz-product.xml
+
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+ PRODUCT_PACKAGES += \
+     FaceUnlock
+ PRODUCT_SYSTEM_EXT_PROPERTIES += \
+     ro.face.sense_service=true
+ PRODUCT_COPY_FILES += \
+     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
+ endif
