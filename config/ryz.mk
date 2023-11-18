cd1b6d9c080879cf2a7b5eead6974d155b64fe1b
aswin7469 <aswinas@pixysos.com>
2023-11-18 10:43:29 +0530

    vendor: Enable Cloned App
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

---
+++ config/ryz.mk
@@ -0,0 +1,3 @@
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-ryz-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-ryz-product.xml
