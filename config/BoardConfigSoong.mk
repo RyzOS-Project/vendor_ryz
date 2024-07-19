d7ec49c86bef4ba0747345a0682c7d0ca0ec550d
AnierinB <anierin@evolution-x.org>
2024-07-19 18:09:00 +0000

    soong: Conditionally spoof ro.product.first_api_level to 32 [1/2]
    Devices that shipped on API 33 or higher need to set
    ro.product.first_api_level to 32 in order to pass integrity.
    
    Set SPOOF_FIRST_API_LEVEL_32 := true in BoardConfig*.mk to enable.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -19,6 +19,13 @@ EXPORT_TO_SOONG := \
 $(call add_soong_config_namespace,lineageVarsPlugin)
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call add_soong_config_var,lineageVarsPlugin,$(v))))
 
+SOONG_CONFIG_NAMESPACES += lineageGlobalVars
+SOONG_CONFIG_lineageGlobalVars += \
+    spoof_first_api_level_32
+
+# Soong bool variables
+SOONG_CONFIG_lineageGlobalVars_spoof_first_api_level_32 := $(SPOOF_FIRST_API_LEVEL_32)
+
 # Camera
 ifneq ($(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED),)
     $(warning TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED is deprecated, please migrate to soong_config_set,camera,override_format_from_reserved)
