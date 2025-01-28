7d2631370413fe3e26b48b63e08584e761b92a12
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-28 20:27:05 +0900

    [SQUASH] Add pihooks config: properties: Add pihooks default values
    config: Add RELEASE and SDK_INT pihooks props
    
    config: properties: Update pihooks to A16 QPR1 Beta 1
    
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -384,6 +384,22 @@ BYPASS_CHARGE_SUPPORTED ?= false
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.battery_bypass_supported=$(BYPASS_CHARGE_SUPPORTED)
 
+# PIF values
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_PRODUCT?=cheetah_beta \
+    persist.sys.pihooks_DEVICE?=cheetah \
+    persist.sys.pihooks_ID?=BP31.250502.008 \
+    persist.sys.pihooks_RELEASE?=12 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-05-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SDK_INT?=32
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PihooksGmsFp="google/cheetah_beta/cheetah:16/BP31.250502.008/13497110:user/release-keys" \
+    PihooksGmsModel="Pixel 7 Pro"
+
 ifeq ($(WITH_GMS),true)
 -include vendor/gapps/arm64/arm64-vendor.mk
 endif
