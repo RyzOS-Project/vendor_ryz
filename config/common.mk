3ef4a29652a19cb46ffb1a69bd0ca38192fa94d5
HeCheng Yu <kde-yyds@qq.com>
2025-02-10 15:26:27 +0800

    Disable default frame rate limit for games
    * Android 15 limits refresh rate to 60hz for games unless we enable
      the developer option 'Disable default frame rate for games' manually.
    * Adding this prop enables it by default.
    
    Change-Id: I336996d07e8594f3f628f4a5999da6cd480aa14e
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -291,6 +291,10 @@ include vendor/lineage/config/version.mk
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-custom-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-custom-product.xml
 
+# Disable default frame rate limit for games
+PRODUCT_PRODUCT_PROPERTIES += \
+    debug.graphics.game_default_frame_rate.disabled=true
+
 ifeq ($(WITH_GMS),true)
 -include vendor/gapps/arm64/arm64-vendor.mk
 endif
