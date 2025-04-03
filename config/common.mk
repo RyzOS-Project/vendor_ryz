336193aba91f098987cf84d5a81fc76c5d509851
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-03 18:18:32 +0800

    config: Add flag for LOS prebuilts inclusion
    Change-Id: Iccfd78e21f7eb2a7bd23d35fe8725df85cafb6d5
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -126,10 +126,14 @@ PRODUCT_PACKAGES += \
     build-manifest
 
 # Lineage packages
-ifeq ($(PRODUCT_IS_ATV),)
+TARGET_INCLUDES_LOS_PREBUILTS ?= false
+ifeq ($(TARGET_INCLUDES_LOS_PREBUILTS),true)
 PRODUCT_PACKAGES += \
     ExactCalculator \
-    Jelly
+    Jelly \
+    Glimpse \
+    Etar \
+    Twelve
 endif
 
 ifeq ($(PRODUCT_IS_AUTOMOTIVE),)
