ClownUI-vic/vendor_clown

vendor: Enable Cloned App
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: John Galt <johngaltfirstrun@gmail.com>
Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>

Signed-off-by: AnierinB <anierin@evolution-x.org>
Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

config/common.mk
@@ -1,5 +1,6 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
+$(call inherit-product, vendor/lineage/config/clown.mk)
 $(call inherit-product, vendor/addons/clown.mk)
 
 # Pixel additions

RyzOS-Project/vendor_ryz

config/common.mk
@@ -1,5 +1,6 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
+$(call inherit-product, vendor/lineage/config/ryz.mk)
 
 PRODUCT_BRAND ?= RyzOS
 