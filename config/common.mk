risingOSS/vendor_lineage

Add preinstalled package list to stop auto cloning
Only extra adds needed, gms now ships it's own lists for this.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

config/common.mk
@@ -257,6 +257,10 @@ CUSTOM_LOCALES += \
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/lineage/build/target/product/security/lineage
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-android-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-android-product.xml
+
 include vendor/lineage/config/version.mk
 
 -include vendor/lineage-priv/keys/keys.mk

RyzOS-Project/vendor_ryz

Change-Id: I46263684a80372120936f91d23f8b7b68a1c511d
Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

config/common.mk
@@ -270,6 +270,10 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 include vendor/lineage/config/version.mk
 
+# Cloned app exemption
+ PRODUCT_COPY_FILES += \
+     vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-custom-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-custom-product.xml
+ 
 # Private keys
 -include vendor/ryz-priv/keys/keys.mk
 